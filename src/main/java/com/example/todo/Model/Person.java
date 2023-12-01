e3b782a223509dd83e1a4168d39370b3eed6e663
Mihail Chaplygin <caplyginmihail48@gmail.com>
2023-12-01 22:33:03 +0300

    change pom and models

--- src/main/java/com/example/todo/Model/Person.java
+++ src/main/java/com/example/todo/Model/Person.java
@@ -1,6 +1,10 @@
 package com.example.todo.Model;
 import jakarta.persistence.*;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Stream;
+
 @Entity
 @Table(name = "person")
 public class Person {
@@ -10,8 +14,23 @@ public class Person {
     private int id;
     @Column(name = "username")
     private String username;
+
     @Column(name = "password")
     private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    @ManyToMany
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Collection<Role> roles;
+
+
+
 
     public int getId() {
         return id;
@@ -37,9 +56,19 @@ public class Person {
         this.password = password;
     }
 
-    public Person(String username, String password) {
+    public Collection<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Collection<Role> roles) {
+        this.roles = roles;
+    }
+
+    public Person(int id, String username, String password, Collection<Role> roles) {
+        this.id = id;
         this.username = username;
         this.password = password;
+        this.roles = roles;
     }
 
     public Person() {
